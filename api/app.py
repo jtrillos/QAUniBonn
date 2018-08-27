4dd3d8004a5109f22a2cfd675c1707de063acd89
Jaime M Trillos <jtrillos@users.noreply.github.com>
2018-08-27 16:31:17 +0200

    Update api taken from dummyserver_QA
    https://github.com/onikolskyy/dummyserver_QA/blob/master/app.py

--- api/app.py
+++ api/app.py
@@ -1,37 +1,19 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response
+from flask import Flask, jsonify, abort, make_response, request
+import numpy as np
+from gensim.models import FastText
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+@app.route('/')
+def hello_world():
+    return "this is a dummy fasttext server"
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
-
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': task[0]})
-
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+@app.route('/getvec/<string:question>')
+def get_vec(question):
+    wordVector = model[question]
+    return str(question)
 
 if __name__ == '__main__':
+	model = FastText.load_fasttext_format('data/wiki.de.bin')
 	app.run(debug=True, host='0.0.0.0')
