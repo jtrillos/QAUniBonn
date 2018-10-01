c1f1a5d414c6d08b7a5100c4ac676adacc53a1a3
jtrillos <jaimemanueltrillos@gmail.com>
2018-10-01 20:16:19 +0200

    updating askQuestion.py

--- Client/askQuestion.py
+++ Client/askQuestion.py
@@ -3,6 +3,7 @@ import requests
 def askQuestion(question):
     req = {'question': question}
     res = requests.post( 'http://localhost:8000/ask',json=req)
+    print res.content
 
 question = input()
 askQuestion(question)
